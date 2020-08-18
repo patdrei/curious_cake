6c216e8c284190c357705fbc0bb198de056717eb
Sarah Harleux <s.harleux@gmail.com>
2020-08-18 11:22:39 +0200

    adding seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_143432) do
+ActiveRecord::Schema.define(version: 2020_08_17_155947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2020_08_17_143432) do
     t.integer "available_slices"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.datetime "eat_by"
     t.index ["user_id"], name: "index_cakes_on_user_id"
   end
 
