12487f2f9d42f7cdc41501dcc87c71750984cced
Jakob Habsburg <jakob.habsburg@icloud.com>
2020-08-20 15:45:17 +0200

    starting searchbar

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -2,4 +2,11 @@ class Cake < ApplicationRecord
   belongs_to :user
   has_many :requests
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+    against: [ :name, :description],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
